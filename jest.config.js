1027fbdeaab4becb1b83fbf13bdaf8a47ae878fc
MccSx <shixiangcool@163.com>
2020-04-24 14:44:19 +0800

    添加tsconfig.test.json

--- jest.config.js
+++ jest.config.js
@@ -10,11 +10,11 @@ module.exports = {
   // coverageReporters: ["text", "lcov"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
   moduleDirectories: ["node_modules"],
-  // globals: {
-  //   "ts-jest": {
-  //     tsConfig: "tsconfig.test.json"
-  //   }
-  // },
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.test.json"
+    }
+  },
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/test/__mocks__/file-mock.js",
