46fcb3a0e60ad4fe82f53fc0bf4ecb1b55f69c0e
MccSx <shixiangcool@163.com>
2020-04-28 18:02:56 +0800

    引入scss，配置相关loader

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./icon.scss";
 import "./icons/home.svg";
 
 interface IconProps {
@@ -7,7 +8,7 @@ interface IconProps {
 
 const Icon: React.FunctionComponent<IconProps> = props => {
   return (
-    <svg>
+    <svg className="xinchen-icon">
       <use xlinkHref={`#${props.name}`}></use>
     </svg>
   );
