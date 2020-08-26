07e4b853b09b4272d2fef37ec93e7b797a55d84f
MccSx <shixiangcool@163.com>
2020-08-26 16:10:59 +0800

    添加icon

--- lib/index.tsx
+++ lib/index.tsx
@@ -5,10 +5,7 @@ import Icon from "./icon/icon";
 ReactDOM.render(
   <div>
     <Icon name="home" className="my-home" />
-    <Icon name="add" fill="#90caf9" />
-    <Icon name="delete" />
-    <Icon name="trashcan" />
-    <Icon name="trashcan" fill="rgb(76, 175, 80)" />
+    <Icon name="setting" />
     <Icon name="loading" spin />
   </div>,
   document.querySelector("#root")
