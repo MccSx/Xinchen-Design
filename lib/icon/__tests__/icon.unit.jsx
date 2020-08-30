7fea0954838028d11b268aef0fd07414ce4f1562
MccSx <shixiangcool@163.com>
2020-08-30 23:10:07 +0800

    测试icon组件点击事件

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,5 +1,6 @@
 import * as rerender from "react-test-renderer";
 import React from "react";
+import { mount } from "enzyme";
 import Icon from "../icon";
 
 describe("测试Icon组件", () => {
@@ -14,5 +15,11 @@ describe("测试Icon组件", () => {
         />
       </svg>
     `);
+  });
+  it("can click", () => {
+    const fn = jest.fn();
+    const component = mount(<Icon onClick={fn} />);
+    component.find("svg").simulate("click");
+    expect(fn).toBeCalled();
   });
 });
