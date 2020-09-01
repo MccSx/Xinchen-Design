f86cb123f4d9dd86fef189c2c03659baedcfd127
MccSx <shixiangcool@163.com>
2020-09-01 17:44:32 +0800

    试着第一次发布

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,12 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Icon from "./icon/icon";
-
-ReactDOM.render(
-  <div>
-    <Icon name="home" className="my-home" />
-    <Icon name="setting" />
-    <Icon name="loading" spin />
-  </div>,
-  document.querySelector("#root")
-);
+export { default as Icon } from "./icon/icon";
